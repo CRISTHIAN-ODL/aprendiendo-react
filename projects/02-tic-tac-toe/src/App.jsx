dcc5a570856c4b6b3ce642044c64f9837ab145c1
CRISTHIAN-ODL <cristhian.codl.10@gmail.com>
2024-12-03 18:11:27 -0500

    Extraer archivos y guardar en el storage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import confetti from "canvas-confetti";
 import { Square } from "./components/square";
 import { TURNS } from "./constants";
 import { checkWinnerForm, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
+import { saveGameToStorage, resetGameStorage } from "./logic/storage";
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(()=> {
+    console.log("Inicializar estado del board");
+    const boardFromStorage = window.localStorage.getItem("board");
+    if(boardFromStorage) return JSON.parse(boardFromStorage);
+    return Array(9).fill(null);
+  });
+  const [turn, setTurn] = useState(()=> {
+    console.log("Inicializar estado del turno");
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X
+  });
   const [winner, setWinner] = useState(null); // null no hay ganador, false empate
   
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    resetGameStorage();
   }
 
   const updateBoard = (index) => {
@@ -27,6 +37,11 @@ function App() {
     //cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O: TURNS.X;
     setTurn(newTurn);
+    //guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    });
     //comprobar si hay ganador
     const newWinner = checkWinnerForm(newBoard)
     if(newWinner){
@@ -37,6 +52,11 @@ function App() {
       setWinner(false);      
     }
   };
+
+  useEffect(()=>{
+    console.log("useEffect");
+  }, [turn]);
+
   return (
   <main className='board'>
     <h1>Tic Tac Toe</h1>
